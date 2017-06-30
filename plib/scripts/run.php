c21e79649069542fd1e179e4dce95653de1ac4b1
Alen Kocaj <alen.kocaj@posteo.at>
2017-06-30 15:12:02 +0200

    Revert falsely assumed used plesk function

--- plib/scripts/run.php
+++ plib/scripts/run.php
@@ -12,17 +12,12 @@ pm_Context::init('nimbusec-agent-integration');
 $varDir = pm_Context::getVarDir();
 $agent = json_decode(pm_Settings::get("agent"), true)["name"];
 
-// define command
-$utility = "agent.sh";
-$args = array();
-
-array_push($args, "{$varDir}/{$agent}");
-array_push($args, "-config", "{$varDir}/agent.conf");
+$cmd = "{$varDir}/{$agent} -config {$varDir}/agent.conf ";
 
 if (pm_Settings::get("agentYara") == "1") {
-	array_push($args, "-yara");
+	$cmd .= "-yara ";
 }
 
-array_push($args, ">", "{$varDir}/agent.log", "2>&1");
+$cmd .= "> {$varDir}/agent.log 2>&1";
 
-pm_ApiCli::callSbin($utility, $args);
+system($cmd);
--- sbin/agent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-while [ $# -gt 0 ]
-do
-    CMD="$CMD $1"
-    shift
-done
-
-eval "$CMD"

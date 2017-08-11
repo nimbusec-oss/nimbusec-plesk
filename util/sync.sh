86eb8e1b483a4bf24062ddba9b560cc870fc598d
Alen Kocaj <alen.kocaj@posteo.at>
2017-08-11 12:20:05 +0200

    add safety measure for dev tool

--- util/sync.sh
+++ util/sync.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-current=$(docker ps --format "{{.ID}}")
+fetched=$(docker ps --no-trunc --quiet --filter "ancestor=plesk/plesk")
+readarray -t instances <<< "$fetched"
+
+if [[ ${#instances[@]} == 2 ]]; then
+	>&2 printf "multiple plesk instances running. abort... \n"
+    exit 1
+fi
+
+current=${instances[0]}
 
 # libraries
 docker cp ../src/plib/library/  $current:/opt/psa/admin/plib/modules/nimbusec-agent-integration/

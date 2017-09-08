8769132a459193d33efe59a661c8fdef44f3f009
Alen Kocaj <alen.kocaj@posteo.at>
2017-09-08 14:47:18 +0200

    Rewrite Agent Controller to use HTML

--- src/plib/controllers/AgentController.php
+++ src/plib/controllers/AgentController.php
@@ -24,70 +24,50 @@ class AgentController extends pm_Controller_Action
 
 		// query store
         $agent = json_decode(pm_Settings::get("agent"), true);
-		
-        $form = new pm_Form_Simple();
-		$form->setMethod("post");
-		$form->setAction($this->_helper->url("update-agent", "agent"));
 
 		$this->view->agent_version 	= $agent["version"];
 		$this->view->agent_os 		= $agent["os"];
 		$this->view->agent_arch 	= $agent["arch"];
-		
+
+		$this->view->agent_outdated = "false";
+
 		// check updateability of agent		
 		$nimbusec = new Modules_NimbusecAgentIntegration_NimbusecHelper();
 		$version = $nimbusec->getNewestAgentVersion($agent["os"], $agent["arch"]);
 
 		if ($version > $agent["version"]) {
-			$form->addControlButtons([
-				"sendTitle" => sprintf(pm_Locale::lmsg("agent.controller.update"), $version),
-				"cancelLink" => pm_Context::getModulesListUrl(),
-			]);
+			$this->view->agent_outdated = "true";
+			$this->view->update_version = $version;
 			$this->_status->addMessage("warning", pm_Locale::lmsg("agent.controller.outdated"));
+
 		} else {
-			$form->addControlButtons([
-				"sendHidden" => true,
-				"cancelLink" => pm_Context::getModulesListUrl(),
-			]);
 			$this->_status->addMessage("info", pm_Locale::lmsg("agent.controller.not_outdated"));
 		}
-
-		$this->view->form = $form;
 	}
 
 	public function updateAgentAction() 
 	{
-		if (!$this->getRequest()->isPost()) {
+		$request = $this->getRequest(); 
+		$valid = Modules_NimbusecAgentIntegration_PleskHelper::isValidPostRequest($request, "submit", "updateAgent");
+		if (!$valid) {
 			$this->_forward("view", "agent");
 			return;
 		}
 
-		$err = false;
-		$msg = pm_Locale::lmsg("agent.controller.updated");
-	
 		$nimbusec = new Modules_NimbusecAgentIntegration_NimbusecHelper();
 		try {
-			if (!$nimbusec->fetchAgent(pm_Context::getVarDir())) {
-				$err = true;
-				$msg = pm_Locale::lmsg("error.download_agent");
-			}
+			// fetch server agent
+			$nimbusec->fetchAgent(pm_Context::getVarDir());
 		} catch (Exception $e) {
-			$err = true;
-			$message = $e->getMessage();
-
-			if (strpos($message, "400") !== false || strpos($message, "401") !== false || strpos($message, "403") !== false) {
-				$msg = pm_Locale::lmsg("error.api_credentials");
-			} elseif (strpos($message, "404") !== false) {
-				$msg = pm_Locale::lmsg("error.agent_not_supported");
-			}
+			pm_Log::err("Downloading server agent failed: {$e->getMessage()}");
+			
+			$this->_status->addMessage("error", pm_Locale::lmsg("error.download_agent"));
+			$this->_forward("view", "agent");
+			return;
 		}
 
-		if (!$err) {
-			$this->_status->addMessage("info", $msg);
-		} else {
-			$this->_status->addMessage("error", $msg);
-		}
-
-		$this->_helper->redirector("view", "agent");
+		$this->_status->addMessage("info", pm_Locale::lmsg("agent.controller.updated"));
+		$this->_helper->redirector->gotoSimple("view", "agent");
 		return;
 	}
 }
--- src/plib/library/PleskHelper.php
+++ src/plib/library/PleskHelper.php
@@ -77,22 +77,26 @@ class Modules_NimbusecAgentIntegration_PleskHelper
 
     public static function isValidPostRequest($request, $form_event = "action", $expected_action, $dynamic_action = false) 
     {
+        // is post
 		if (!$request->isPost()) {
 			return false;
 		}
 
+        // does the given form element exist
 		$fetched_action = $request->getPost($form_event);
 		if (!isset($fetched_action)) {
 			return false;
 		}
 
         if (!$dynamic_action) {       
+            // does the form element equals the given action
             if ($fetched_action !== $expected_action) {
                 return false;
             }
         }
 
         if ($dynamic_action) {
+            // does the form element starts with the given action
             if (strpos($fetched_action, $expected_action) === false) {
                 return false;
             }
